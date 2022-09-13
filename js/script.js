15e1a1e756837f4390967df409ed7feffc4de23f
Jakub Zuber <jakub.zuber19@gmail.com>
2022-09-13 22:42:51 +0200

    bug fix from YC

--- js/script.js
+++ js/script.js
@@ -6,15 +6,15 @@
 };
 
 { /* video button */
-    const youtubeHidden = document.querySelector(".section__videoButton--videoHidden");
+    const youtubeHidden = document.querySelector(".section__iframe");
     const videoButton = document.querySelector(".section__videoButton");
 
     const onChangeVisibilityButton = () => {
-        youtubeHidden.classList.toggle("section__videoButton--videoVisible");
+        youtubeHidden.classList.toggle("section__iframe--Visible");
     }
 
     const onChangeVideoButtonText = () => {
-        videoButton.innerHTML = youtubeHidden.classList.contains("section__videoButton--videoVisible") ? "My first video<br>(click to close)" : "My first video<br>(click to see)"
+        videoButton.innerHTML = youtubeHidden.classList.contains("section__iframe--Visible") ? "My first video<br>(click to close)" : "My first video<br>(click to see)"
     }
     
     
