c2c34b4df2daeb0b5a7123da1830ac6e542fea97
Jakub Zuber <jakub.zuber19@gmail.com>
2022-09-13 16:21:39 +0200

    js update

--- js/script.js
+++ js/script.js
@@ -1,16 +1,25 @@
-{
+{ /* console welcome message */
     const welcome = () => {
         console.log("Witam na mojej stronie")
     }
     welcome();
 };
-{ 
-    const onChangeVisibilityAndTextButton = () => {
-        const youtubeHidden = document.querySelector(".section__videoButton--videoHidden");
+
+{ /* video button */
+    const youtubeHidden = document.querySelector(".section__videoButton--videoHidden");
+    const videoButton = document.querySelector(".section__videoButton");
+
+    const onChangeVisibilityButton = () => {
         youtubeHidden.classList.toggle("section__videoButton--videoVisible");
+    }
+
+    const onChangeVideoButtonText = () => {
         videoButton.innerHTML = youtubeHidden.classList.contains("section__videoButton--videoVisible") ? "My first video<br>(click to close)" : "My first video<br>(click to see)"
     }
     
-    const videoButton = document.querySelector(".section__videoButton");
-    videoButton.addEventListener("click", onChangeVisibilityAndTextButton);
+    
+    videoButton.addEventListener("click", () => {
+    onChangeVisibilityButton();
+    onChangeVideoButtonText();
+    })
 };
